5a4da571380bbdbbccf283b84df6038fa216166f
Tatsuya Fukata <tatsuya.fukata@gmail.com>
2013-08-12 11:25:03 +0900

    implemented v0.1

--- app.go
+++ app.go
@@ -1,21 +1,119 @@
 package main
 
 import (
+    "log"
     "fmt"
     "net/http"
     "strconv"
     "flag"
     "time"
+    "io/ioutil"
+    "encoding/json"
 )
 
 type Dsp struct {
-    sleep_ms int
-    status   int
-    price    int
+    DspId   int
+    ReqId   string 
+    SleepMs int
+    Status   int
+    Price    int
 }
 
-func doRequest(Dsp *dsp) {
-    time.Sleep( time.Second * 2 )
+// {"id":"gZtaC8svEsDMyXYUukU3GM62gRPZm4mAT7MNINjrnTB3l47gBFbRxmeI4djNejm1","status":1,"price":1000}
+type BidResponse struct {
+    Id     string
+    Status int
+    Price  int
+}
+
+type Result struct {
+    DspId  int
+    ReqId  string
+    Status  int
+    Price   int
+}
+
+type Win struct {
+    DspId  int
+    ReqId  string
+    Price  int
+}
+
+type Response struct {
+    Id      string      `json:"id"`
+    DspId   int         `json:"dsp_id"`
+    Price   int         `json:"price"`
+}
+
+func makeClient() http.Client {
+    transport := http.Transport{
+        ResponseHeaderTimeout: time.Millisecond * 120,
+    }
+
+    client := http.Client{
+        Transport: &transport,
+    }
+
+    return client
+}
+
+func doRequests(dsps []Dsp) <-chan Result {
+    receiver := make(chan Result, len(dsps))
+
+    for _, dsp := range dsps {
+        go doRequest(dsp, receiver)
+    }
+
+    return receiver
+}
+
+func doRequest(dsp Dsp, receiver chan Result) {
+    client := makeClient()
+    url := fmt.Sprintf("http://localhost:8080/ad?id=%s&t=%d&s=%d&p=%d", dsp.ReqId, dsp.SleepMs, dsp.Status, dsp.Price)
+    //log.Println(url)
+    resp, err := client.Get(url)
+
+    result := Result{}
+    if err != nil {
+        //log.Println("error")
+        result.DspId = dsp.DspId
+    } else {
+        defer resp.Body.Close()
+        body, _ := ioutil.ReadAll(resp.Body)
+        //log.Println(string(body))
+        var bid_res BidResponse
+        err := json.Unmarshal(body, &bid_res)
+        if err != nil {
+            //log.Println("json parse error")
+        }
+
+        result.DspId  = dsp.DspId
+        result.ReqId  = bid_res.Id
+        result.Status = bid_res.Status
+        result.Price  = bid_res.Price
+    }
+
+    receiver <- result
+}
+
+func doAuction(results []Result) Win {
+    win := Win{}
+    var max_result Result
+    for _, result := range results {
+        if result.Status != 1 { continue }
+
+        if max_result.Price == 0 {
+            max_result = result
+        } else if result.Price > max_result.Price {
+            max_result = result
+        }
+    }
+
+    win.DspId = max_result.DspId
+    win.ReqId = max_result.ReqId
+    win.Price = max_result.Price
+
+    return win
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -34,12 +132,39 @@ func handler(w http.ResponseWriter, r *http.Request) {
             price, _ = strconv.Atoi( params[price_key][0] )
         }
 
-        dsp     := Dsp{ sleep_ms, status, price }
+        dsp     := Dsp{ i, id, sleep_ms, status, price }
         dsps[i] = dsp
     }
 
+    // do request to dsps
+    results  := make([]Result, len(dsps))
+    receiver := doRequests(dsps)
+
+    // receive result
+    result_num := 0
+    for {
+        result := <-receiver
+        //log.Println(result)
+        results[result.DspId] = result
+
+        result_num++
+        if len(results) == result_num { break }
+    }
+    //log.Println(results)
+
+    win := doAuction(results)
+    //log.Println(win)
+
     w.Header().Set("Content-Type", "application/json")
-    fmt.Fprintf(w, "{\"id\":\"%s\"}", id)
+    response := Response{
+        Id: win.ReqId,
+        DspId: win.DspId,
+        Price: win.Price,
+    }
+    bytes, _ := json.Marshal(response)
+    json_str := string(bytes)
+    log.Println(json_str)
+    fmt.Fprint(w, json_str)
 }
 
 func main() {
