eaef5d4ed5c8f0fc6304b69de2e104bd55b72d04
Tatsuya Fukata <tatsuya.fukata@gmail.com>
2013-08-12 14:51:24 +0900

    とりあえず

--- app.go
+++ app.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-//    "log"
+    "log"
     "fmt"
     "net/http"
     "strconv"
@@ -10,6 +10,62 @@ import (
     "io/ioutil"
     "encoding/json"
 )
+
+type RTBHTTPHandler struct {
+    m *http.Handler
+}
+
+func (h *RTBHTTPHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+    params     := r.URL.Query()
+    id         := params["id"][0]
+    dsp_num, _ := strconv.Atoi( params["dsp"][0] )
+    dsps       := make([]Dsp, dsp_num)
+
+    // parse dsp parameters 
+    for i := 0; i < dsp_num; i++ {
+        sleep_ms, _ := strconv.Atoi( params[fmt.Sprintf("d%d_t", i)][0] )
+        status, _   := strconv.Atoi( params[fmt.Sprintf("d%d_s", i)][0] )
+        price       := 0
+        price_key   := fmt.Sprintf("d%d_p", i)
+        if params[price_key] != nil {
+            price, _ = strconv.Atoi( params[price_key][0] )
+        }
+
+        dsp     := &Dsp{ i, id, sleep_ms, status, price }
+        dsps[i] = *dsp
+    }
+
+    // do request to dsps
+    results  := make([]Result, len(dsps))
+    receiver := doRequests(dsps)
+
+    // receive result
+    result_num := 0
+    for {
+        result := <-receiver
+        //log.Println(result)
+        results[result.DspId] = result
+
+        result_num++
+        if len(results) == result_num { break }
+    }
+    //log.Println(results)
+
+    win := doAuction(results)
+    //log.Println(win)
+
+    w.Header().Set("Content-Type", "application/json")
+    response := Response{
+        Id: win.ReqId,
+        DspId: win.DspId,
+        Price: win.Price,
+    }
+    bytes, _ := json.Marshal(response)
+    json_str := string(bytes)
+//    log.Println(json_str)
+    fmt.Fprint(w, json_str)
+
+}
 
 type Dsp struct {
     DspId   int
@@ -171,6 +227,14 @@ func main() {
     port := flag.Int("port", 5000, "PORT")
     flag.Parse()
 
-    http.HandleFunc("/ad", handler)
-    http.ListenAndServe(fmt.Sprintf(":%d", *port), nil)
+    rtbHandler := RTBHTTPHandler{}
+    s := &http.Server{
+        Addr:    fmt.Sprintf(":%d", *port),
+        Handler: &rtbHandler,
+        ReadTimeout: 500 * time.Millisecond,
+    }
+
+    //http.HandleFunc("/ad", handler)
+    //log.Fatal( http.ListenAndServe(fmt.Sprintf(":%d", *port), nil) )
+    log.Fatal( s.ListenAndServe() )
 }
